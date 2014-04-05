de7a578a85391acb3c797773e7023ae490ae58eb
arjunv1512 <arjunvaju@gmail.com>
2014-04-05 19:40:41 +0530

    Modofied mainfn,removed unnecessary image displays,optimised code. You need to edit the paths in matlab function. It is by default set inside eclipse directory(for further processing)

--- ip_ver_1/mainfn.m
+++ ip_ver_1/mainfn.m
@@ -1,33 +1,23 @@
-
-imagen=imread('s8.png');
-imshow(imagen);
-title('INPUT IMAGE WITH NOISE')
+imagen=imread('C:\Users\arjun\Documents\eclipse\mainfn\data\input\sample.png');
 
 if size(imagen,3)==3 
     imagen=rgb2gray(imagen);
 end
 threshold = graythresh(imagen);
 imagen =~im2bw(imagen,threshold);
-imagen = bwareaopen(imagen,30);
-pause(1);
-imshow(~imagen);
-title('INPUT IMAGE WITHOUT NOISE')
+imagen = bwareaopen(imagen,50);
 [L Ne]=bwlabel(imagen);
 propied=regionprops(L,'BoundingBox');
-hold on
-for n=1:size(propied,1)
-    rectangle('Position',propied(n).BoundingBox,'EdgeColor','g','LineWidth',2)
-end
-hold off
-pause (1)
-bpath='%d.bmp';
-figure
+  path='C:\Users\arjun\Documents\eclipse\mainfn\data\segmented';
+  if ~exist(path, 'dir')
+  mkdir(path);
+  end
+  delete('C:\Users\arjun\Documents\eclipse\mainfn\data\segmented\*.bmp');
 for n=1:Ne
     [r,c] = find(L==n);
     n1=imagen(min(r):max(r),min(c):max(c));
-    imshow(~n1);
     n2=imresize(~n1,[16 16]);
-    path=sprintf(bpath,n);
-    imwrite(n2,path,'bmp');
-    pause(0.2);
+     baseFileName = sprintf('img_%d.bmp', n);
+  fullFileName = fullfile(path, baseFileName);
+  imwrite(n2, fullFileName);
 end
