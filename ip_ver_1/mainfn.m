2835dfa4b78684a0efabd4e239eaa89ad5ae829b
arjunv1512 <arjunvaju@gmail.com>
2014-04-11 22:46:53 +0530

    The updated image processing module now contains a line segmentation algorithm. The line segmentation will segment entire image based on lines, and these line segmented images are then further segmented based on character. Th end result preserves the order of characters as supplied in the input image.

--- ip_ver_1/mainfn.m
+++ ip_ver_1/mainfn.m
@@ -1,4 +1,5 @@
-imagen=imread('C:\Users\arjun\Documents\eclipse\mainfn\data\input\sample.png');
+function y=mainfn(p)
+imagen=imread('C:\Users\arjun\Documents\eclipse\mainfn\data\input\sam.png');
 
 if size(imagen,3)==3 
     imagen=rgb2gray(imagen);
@@ -8,16 +9,19 @@ imagen =~im2bw(imagen,threshold);
 imagen = bwareaopen(imagen,50);
 [L Ne]=bwlabel(imagen);
 propied=regionprops(L,'BoundingBox');
+
   path='C:\Users\arjun\Documents\eclipse\mainfn\data\segmented';
   if ~exist(path, 'dir')
   mkdir(path);
   end
-  delete('C:\Users\arjun\Documents\eclipse\mainfn\data\segmented\*.bmp');
+ 
+y=p;
 for n=1:Ne
     [r,c] = find(L==n);
     n1=imagen(min(r):max(r),min(c):max(c));
     n2=imresize(~n1,[16 16]);
-     baseFileName = sprintf('img_%d.bmp', n);
+     baseFileName = sprintf('img_%d.bmp', n+p);
   fullFileName = fullfile(path, baseFileName);
   imwrite(n2, fullFileName);
+  y=y+1;
 end
